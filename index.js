9c043621e104c840d082de062ac8854c06eeba1e
html <newparas11422@gmail.com>
2022-06-08 18:57:03 +0530

    function

--- index.js
+++ index.js
@@ -1,75 +1,72 @@
-// general Loops: for, while, do-while
+const name = 'Paras'
+const name2 = 'Anuj'
 
-for (i = 0; i < 10; i++) {
-    // if(i===9){
-    //     i++;
-    //     continue
-    // }
-    if(i===2){break}
-    // console.log(i)
+// console.log(`“Hi! Welcome to ${name}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!”`);
+
+// console.log(`“Hi! Welcome to ${name2}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!”`);
+
+const func =(nameVal, greeting='good morning')=>{
+   
+     let msg =`“Hi! Welcome to ${nameVal}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!” ${greeting.toUpperCase()}`
+     return msg
+}
+// let val = func('html',name2)
+// let val2 = func('nothing')
+// console.log(val);
+
+let hello = function wish(e,id) {
+    let msg = `“Hi! Welcome to ${e}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!” ${id.toUpperCase()}`
+    return msg;
+}
+// let func1 = wish('paras','good bye')
+// console.log(func1);
+
+// console.log(hello('done','bye')) 
+
+const obj ={
+    name: 'paras',
+    age: 3333,
+    run: paras=()=>{ return 'hi'},
 }
 
-let a = 10
-a++
-a += 10 /*it means a = a+10*/
-a -= 10 /*it means a = a-10*/
-// console.log(a)
+/*both has same output */
+// console.log(obj.run()); 
+// console.log(paras());
 
-let x = 10
-let y = 20
-x -= y /*it means x = x-y*/
-y += x /*it means x = x+y*/
-// console.log('x =',x,'y =',y)
-
-// while loop
-
-let w = 0;
-while (w < 10) {
-    if (w === 1) {
-        // break;
-        w++;
-        continue;
-
-    }
-    // console.log(w)
-
-    w += 1;
-
-}
-
-
-//do while loop
-k = 0
-do {
-
-    if (k === 5) {
-        // break;
-        k++
-        continue;
-    }
-    // console.log(k)
-    k++;
-
-} while (k < 10);
-
-let arrayy =['hello','done','loop',23,44]
-for (let i = 0; i < arrayy.length; i++) {
-    const e = arrayy[i]
-    // console.log(e);
-    
-}
-arrayy.forEach((e,index, array)=>{
-    console.log(e);
-    console.log(index);
-    console.log(array);
+const arr =['orange','apple','banana']
+arr.forEach((e,index)=>{
+// console.log(e,index);
 })
 
-let intro ={
-    name: 'paras',
-    age:999,
-    live:'delhi'
+//try var instead of let for learning
+if(1){
+    let i = 234;
+    console.log(i);
 }
+let i = 'PARAS'
+console.log(i);
 
-for(let key in intro){
-    // console.log(key);
+function ui(name) {
+    let i = 9;
+    console.log(i);
+    return `this is a ${name} ui`
 }
+console.log(ui('paras'),i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
