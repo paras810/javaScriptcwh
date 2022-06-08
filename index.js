15b7db4e1ef9dd10dc29bf92e2eb80bb763c9036
html <newparas11422@gmail.com>
2022-06-09 02:59:56 +0530

    understanding dom

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-console.log('This is tut 11');
-let a = window.document;
-// alert('hello harry');
-// a = prompt('This will destroy your computer. Type your name');
+let a= document
+a = document.all
+// a = document.body
+// a = document.forms
+// Array.from(a).forEach(e => {
+//     console.log(a);
+// });
 
-// a = confirm('Are you sure you want to delete this page?');
-a = window.innerHeight;
-a = innerWidth;
-a = outerWidth;
-a = scrollY;
-a = location.href;
-// location.reload()
-// location.href = '//youtube.com'
-a = location.toString();
-// a = window.history;
-// history.go(1)
-// history.go(-1)
-console.log(a);          
+// a  = document.links
+// console.log(a[0]);
+// console.log(a[0].href);
+
+console.log(a);
+
