a38b2c5944dc790c71f9c042f92577803a5bd6e7
html <newparas11422@gmail.com>
2022-06-09 01:51:11 +0530

    window object

--- index.js
+++ index.js
@@ -1,72 +1,18 @@
-const name = 'Paras'
-const name2 = 'Anuj'
+console.log('This is tut 11');
+let a = window.document;
+// alert('hello harry');
+// a = prompt('This will destroy your computer. Type your name');
 
-// console.log(`“Hi! Welcome to ${name}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!”`);
-
-// console.log(`“Hi! Welcome to ${name2}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!”`);
-
-const func =(nameVal, greeting='good morning')=>{
-   
-     let msg =`“Hi! Welcome to ${nameVal}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!” ${greeting.toUpperCase()}`
-     return msg
-}
-// let val = func('html',name2)
-// let val2 = func('nothing')
-// console.log(val);
-
-let hello = function wish(e,id) {
-    let msg = `“Hi! Welcome to ${e}. We will get back to you within 3 hours. In the meantime, you can check out our help center. We have put a lot of effort into it, so the answer to your question may already be in there!” ${id.toUpperCase()}`
-    return msg;
-}
-// let func1 = wish('paras','good bye')
-// console.log(func1);
-
-// console.log(hello('done','bye')) 
-
-const obj ={
-    name: 'paras',
-    age: 3333,
-    run: paras=()=>{ return 'hi'},
-}
-
-/*both has same output */
-// console.log(obj.run()); 
-// console.log(paras());
-
-const arr =['orange','apple','banana']
-arr.forEach((e,index)=>{
-// console.log(e,index);
-})
-
-//try var instead of let for learning
-if(1){
-    let i = 234;
-    console.log(i);
-}
-let i = 'PARAS'
-console.log(i);
-
-function ui(name) {
-    let i = 9;
-    console.log(i);
-    return `this is a ${name} ui`
-}
-console.log(ui('paras'),i);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight;
+a = innerWidth;
+a = outerWidth;
+a = scrollY;
+a = location.href;
+// location.reload()
+// location.href = '//youtube.com'
+a = location.toString();
+// a = window.history;
+// history.go(1)
+// history.go(-1)
+console.log(a);          
