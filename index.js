cfc972c7cc454e6c2df39582898aab6c6941f90f
html <newparas11422@gmail.com>
2022-06-10 04:05:39 +0530

    events

--- index.js
+++ index.js
@@ -1,40 +1,18 @@
-let element = document.createElement('li')
+let click = document.getElementById('click')
+click.addEventListener('click', function(e){
+    let variable ;
+    console.log('click');
+    // console.log(typeof e);
+    variable = e.target;
+    variable = e.target.id
+    variable = e.target.className
+    variable = e.target.classList
+    variable = Array.from(e.target.classList);
+    variable = e.offsetX
+    variable = e.offsetY
+    variable = e.clientX
+    variable = e.clientY
 
-element.className = 'createClass';
-element.id = 'createId';
-element.setAttribute('title','hello')
-// element.textContent= 'Created Text'
-let elmTxt = document.createTextNode('Created Text')
-element.appendChild(elmTxt)
+    console.log(variable);
 
-let ul = document.querySelector('ul.this')
-// ul.appendChild(element)
-ul.appendChild(element).style.backgroundColor= 'yellow'
-console.log(element);
-
-
-let newh1 = document.createElement('h1')
-newh1.className = 'H1Class'
-newh1.id = 'H1Id'
-let h1Text = document.createTextNode('Heading')
-newh1.appendChild(h1Text)
-
-// document.querySelector('ul.this').firstElementChild.nextElementSibling.appendChild(newh1)
-document.querySelector('ul.this').appendChild(newh1).style.background = '#fc3903'
- element.replaceWith(newh1)
-
- let first = document.getElementById('first')
- let third = document.getElementById('third')
-//  third.replaceWith(first)
-
-let newChild = document.createElement('h6')
-let h6Text = document.createTextNode('Heading6')
-newChild.appendChild(h6Text)
-
-ul.replaceChild(newChild,first )
-// ul.replaceChildren(newChild,first )
-// ul.removeChild(third)
-
-let getAttr = newh1.getAttribute('id') 
-getAttr = newh1.getAttribute('class') 
-console.log(getAttr);
+})
