ddde03227972a36cb1303d2a5ea076c8a45216da
html <newparas11422@gmail.com>
2022-06-10 18:18:54 +0530

    localStorage and session storage

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-let btn = document.getElementById('btn')
-let container = document.querySelector('.container')
-const func1 =(e)=>{
-    // console.log('thanks', e.offsetX);
-    e.preventDefault()
-    container.style.backgroundColor = `rgba(${e.offsetX} ${e.offsetY} ${e.clientX+20} )`
-    }
-const func2=(e)=>{
-    // console.log('thanks', e.offsetX);
-    e.preventDefault()
-    container.style.backgroundColor = ``
-    }
-// btn.addEventListener('click', func1)
-container.addEventListener('mousemove', func1)
-container.addEventListener('mouseleave', func2)
-// btn.addEventListener('dblclick', func1) run again
+// localStorage.setItem('name','paras')
+// localStorage.setItem('age',999)
+// localStorage.clear()
+// localStorage.removeItem('age')
 
+let getLocalStrValName = localStorage.getItem('name')
+let getLocalStrValAge = localStorage.getItem('age')
+// localStorage.clear()
+// console.log(getLocalStrValName);
+// console.log(getLocalStrValAge);
+
+
+// adding array in local storage
+let array = ['orange', 'banana','apple','mango']
+localStorage.setItem('fruitsList', JSON.stringify(array))
+let stringToArray =  JSON.parse(localStorage.getItem('fruitsList'))
+
+// console.log(stringToArray );
+// console.log(stringToArray[0]);
+
+// sessionStorage.setItem('name','Sparas')
+// sessionStorage.setItem('age',111)
+// sessionStorage.setItem('no',222)
+// sessionStorage.setItem('yes',333)
+// sessionStorage.getItem('name')
+// console.log(sessionStorage.getItem('name'));
+
+
+
+//fun
+// let obj = {type: 'object'}
+// let string = JSON.stringify(obj)
+// console.log(string ,' type of ',typeof string);
+// let backToObj = JSON.parse(string)
+// console.log(backToObj ,' type of ',typeof backToObj);
