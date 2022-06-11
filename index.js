6c20958dcbe019e1522564c695711fdb3f74d2f4
html <newparas11422@gmail.com>
2022-06-11 17:59:57 +0530

    oops basic

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-console.log(Math);
-let number =6.63
-number =6.55
-let number2 =-2
-console.log(Math.round(number))
-console.log(Math.ceil(number))
-console.log(Math.floor(number))
-console.log(Math.abs(number2))
-console.log(Math.sqrt(4));
-console.log(Math.pow(2,4));
-console.log(Math.max(33,33,33,999,22));
-console.log(Math.min(33,33,33,999,22));
-console.log(Math.ceil(Math.random()*5));
+let car = {
+    name: 'volvo',
+    topSpeed: '444 km/h',
+    run: function() {console.log('car is running'); }
+}
 
+function carGenerator(givenName, givenSpeed) {
+    this.carName = givenName,
+    this.carSpeed = givenSpeed,
+    this.run = function(){console.log(`${this.carName} is running on speed ${this.carSpeed} km/h `)},
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
-  console.log(getRndInteger(4,8))
+    this.analyze = function(){ console.log(`${this.carName} is slower by ${378 - this.carSpeed} km/h than tesla`)}
+  
+}
+console.log(car);
+let car1 = new carGenerator('volvo 2.0',200)
+car1 = new carGenerator('Nano',100)
+// console.log(car1);
+// console.log(car1.run());
+console.log(car1.analyze());
+
