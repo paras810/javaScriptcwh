da74e50315a07dd3e61354bd6c36f1dfb0de75ef
html <newparas11422@gmail.com>
2022-06-11 16:17:59 +0530

    Math

--- index.js
+++ index.js
@@ -1,35 +1,19 @@
-// localStorage.setItem('name','paras')
-// localStorage.setItem('age',999)
-// localStorage.clear()
-// localStorage.removeItem('age')
-
-let getLocalStrValName = localStorage.getItem('name')
-let getLocalStrValAge = localStorage.getItem('age')
-// localStorage.clear()
-// console.log(getLocalStrValName);
-// console.log(getLocalStrValAge);
-
-
-// adding array in local storage
-let array = ['orange', 'banana','apple','mango']
-localStorage.setItem('fruitsList', JSON.stringify(array))
-let stringToArray =  JSON.parse(localStorage.getItem('fruitsList'))
-
-// console.log(stringToArray );
-// console.log(stringToArray[0]);
-
-// sessionStorage.setItem('name','Sparas')
-// sessionStorage.setItem('age',111)
-// sessionStorage.setItem('no',222)
-// sessionStorage.setItem('yes',333)
-// sessionStorage.getItem('name')
-// console.log(sessionStorage.getItem('name'));
+console.log(Math);
+let number =6.63
+number =6.55
+let number2 =-2
+console.log(Math.round(number))
+console.log(Math.ceil(number))
+console.log(Math.floor(number))
+console.log(Math.abs(number2))
+console.log(Math.sqrt(4));
+console.log(Math.pow(2,4));
+console.log(Math.max(33,33,33,999,22));
+console.log(Math.min(33,33,33,999,22));
+console.log(Math.ceil(Math.random()*5));
 
 
-
-//fun
-// let obj = {type: 'object'}
-// let string = JSON.stringify(obj)
-// console.log(string ,' type of ',typeof string);
-// let backToObj = JSON.parse(string)
-// console.log(backToObj ,' type of ',typeof backToObj);
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+  console.log(getRndInteger(4,8))
