6d19eac464a638af37803494a002368e0f42651a
html <newparas11422@gmail.com>
2022-06-22 21:32:19 +0530

    ajax GET request

--- index.js
+++ index.js
@@ -1,47 +1,28 @@
-class employee {
-    constructor(givenName, givenExp, givenDiv) {
-        this.name = givenName,
-            this.exp = givenExp,
-            this.div = givenDiv
-    }
-    slogan() {
-        return `hello my name is ${this.name}`
-    }
-    joiningYear() {
-        return `${this.name} joined this company in ${2022 - this.exp}`
-    }
-    static sum(a, b) {
-        return a + b
+let fetchBtn = document.getElementById('fetchBtn')
+fetchBtn.addEventListener('click', buttonClickHandler)
+
+function  buttonClickHandler(){
+console.log('clicked');
+
+// instantiate an xhr object
+const xhr = new XMLHttpRequest()
+
+//open the object
+xhr.open('GET','./paras.text', true)
+
+//what to do on progress (optional)
+xhr.onprogress = function(){
+    console.log('on progress');
+}
+// what to do when response is ready
+xhr.onload = function(){
+    if(this.status === 200){
+    console.log(this.responseText);
+    }else{
+        console.error('some error occured');
     }
 }
-// inheritence
-class programmer extends employee {
-    constructor(givenName, givenExp, givenDiv, givenLan, givenGithub) {
-        super(givenName, givenExp, givenDiv)
-        this.lang = givenLan
-        this.github = givenGithub
-    }
-    favLang(){
-        if(this.lang === 'javaScript'){
-            return `${this.name} favourite language is JavaScript`
-        }
-        else{
-            return `${this.name} favourite language is python` 
-        }
-    }
-    static multiply (x, z){
-        return x * z
-    }
-
+//send the request
+xhr.send()
+console.log('we are done');
 }
-
-let paras = new employee('paras', 1, 'plazma')
-// console.log(paras);
-// console.log(paras.slogan());
-// console.log(paras.joiningYear());
-// console.log(employee.sum(1,12));
-
-let mohan = new programmer('mohan',2,'division','javaScript','mohan3333')
-console.log(mohan);
-// console.log(mohan.favLang());
-console.log(mohan.slogan());
