db86fc5c04756b109e61f8c949fc62c528b7f441
html <newparas11422@gmail.com>
2022-06-27 23:19:11 +0530

    callback function

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
-let fetchBtn = document.getElementById('fetchBtn')
-fetchBtn.addEventListener('click', buttonClickHandler)
+const students = [
+    { name: 'paras', subject: 'javascript' }
+    , { name: 'anuj', subject: 'php' }
+]
 
-function  buttonClickHandler(){
-console.log('clicked');
-
-// instantiate an xhr object
-const xhr = new XMLHttpRequest()
-
-//open the object
-xhr.open('GET','./paras.text', true)
-
-//use this for post
-xhr.open('POST','https://dummy.restapiexample.com/api/v1/create', true)
-// xhr.getResponseHeader('Content-type','application/x-www-form-urlencoded')
-xhr.getResponseHeader('Content-type','application/json')
-
-
-//what to do on progress (optional)
-xhr.onprogress = function(){
-    console.log('on progress');
+function enrollStudent(student, callback) {
+    setTimeout(() => {
+        students.push(student)
+        console.log('student has been enrolled');
+        callback()
+    }, 3000);
 }
-// what to do when response is ready
-xhr.onload = function(){
-    if(this.status === 200){
-    console.log(this.responseText);
-    }else{
-        console.log('some error occured');
-    }
+
+function getStudents() {
+    setTimeout(() => {
+        let str = ''
+        students.forEach(e => {
+            str += `<li> ${e.name} </li>`
+        });
+        document.getElementById('students').innerHTML = str
+        console.log('students have been fetched');
+    }, 1000);
 }
-//send the request
-params = `{"name":"test1","salary":"12","age":"21"}`
-xhr.send(params)
-console.log('we are done');
-}
+let newStudent = { name: 'rohan', subject: 'vue.js' }
+enrollStudent(newStudent, getStudents)
