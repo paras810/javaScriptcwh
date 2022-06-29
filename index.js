9acd306cca3dc632def357779b46bbe318492882
html <newparas11422@gmail.com>
2022-06-29 21:40:10 +0530

    async/ await

--- index.js
+++ index.js
@@ -1,53 +1,15 @@
-let myBtn = document.getElementById('myBtn')
-let content = document.getElementById('content')
-
-// function getData() {
-//     console.log('started getData');
-//     let url = 'paras.text'
-//     fetch(url).then((response)=>{
-//         console.log('inside first then');
-//         return response.text()
-//     }).then((data)=>{
-//         console.log('inside second then');
-//         console.log(data);
-//     })
-// }
-// console.log('before running getData');
-// getData()
-// console.log('after running getData');
-
-// function getData() {
-//     console.log('started getData');
-//     let url = 'https://api.github.com/users'
-//     fetch(url).then((response)=>{
-//         console.log('inside first then');
-//         return response.json()
-//     }).then((data)=>{
-//         console.log('inside second then');
-//         console.log(data);
-//     })
-// }
-// console.log('before running getData');
-// getData()
-// console.log('after running getData');
-
-function postData() {
-    let url = 'https://jsonplaceholder.typicode.com/posts'
-    let data = ``
-    let params ={
-        method: 'post'
-        ,headers:{
-            'content-Type':'application/json'
-        },
-        // use this if data is in json
-        // body: JSON.stringify(data)
-         // use this if data is in string
-        body: data
-    }
-    fetch(url, params).then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        console.log(data );
-    })
+async function paras (){
+    console.log('inside paras function');
+    const response = await fetch('https://api.github.com/users')
+    console.log('before response');
+    const users = await response.json()
+    console.log('users resolved');
+    return users
 }
-postData()
+
+console.log('before calling paras');
+let a = paras()
+console.log('after calling paras');
+console.log(a);
+a.then( function(params){console.log(params)})
+console.log('last line of this file');
