2b98d68a8773004024b911dc9a8d32800ded1403
html <newparas11422@gmail.com>
2022-07-19 23:04:54 +0530

    for in and of loop

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-//  generator in Js 
-//  1-1B
+let peole = ['paras','rohan','anu','skillF']
+// for (let index = 0; index < peole.length; index++) {
+//     const element = peole[index];
+//     console.log(element);
+// }
 
-function* numbersGen() {
-    let i = 0;
-
-    // yield 1;
-    // yield 2;
-    // yield 3;
-    // yield 4;
-    while (true) {
-        // yield i++
-        yield (i++).toString()
-    }
+let obj ={
+    name: 'paras',
+    language: 'JS',
+    hobbies: 'web development'
 }
-let gen = numbersGen();
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next().value);
-console.log(gen.next());
+// console.log(obj);
+// Iterating an object using Traditional for loop
+// for (let index = 0; index < Object.keys(obj).length; index++) {
+//     const element = obj[Object.keys(obj)[index]];
+//     console.log(element);
+// }
+// for (const key in obj) {
+//    console.log(obj[key]);
+// }
+
+let myString = 'This is my string';
+// for (const char in myString) {
+//         console.log(myString[char]);
+// }
+
+let anotherString = 'This is another string'
+// for (const key in anotherString) {
+//         const e = anotherString[key];
+//         console.log(e);
+// }
+
+// FOR OF LOOP 
+
+for (const name of peole) {
+    console.log(name);
+}
+// for (const string of myString ) {
+//     console.log(string);
+// }
