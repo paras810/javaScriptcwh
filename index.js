c7db3c6937d36a1496757689b7bb59a0ad77e5cb
html <newparas11422@gmail.com>
2022-07-23 17:57:03 +0530

    regular expression (Quantifier)

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
-let reg = /paras/
-// reg = /paras/g
-// reg = /paras/i
-// console.log(reg);
-// console.log(reg.source)
+ let reg = /paras/;
+ // let look into some metacharacter symbols
+reg = /^str/ // ^ string start with
+reg = /as$/ // $ string end with 
+reg = /pa.as/ // . any one character if more dot then more char.
+reg = /p*ras/ // * 0 or more characters
+reg = /pa?rasz?t/ // optional
+reg = /paras\^/ // for special characters
 
-//functions to match expression
-let str = " done (paras) this is great done code with paras done"
+ let str = 'par is paras^'
+ 
+ let r = reg.exec(str)
+ console.log(r);
 
-//1. exec() - this function will return an array for match or null for no match
-
-let result = reg.exec(str)
-// result = reg.exec(str)
-// console.log(result);
-// result = reg.exec(str)
-// console.log(result); //we can use multiple exec with global flag
-
-// if (result) {
-//     console.log(result);
-//     console.log(result.input);
-//     console.log(result.index);
-// }
-
-
-// 2. test() - returns true or false
-
-let result2 = reg.test(str)
-// console.log(result2); //this will only print true if the "reg" is there in the string "str"
-
-// 3. match() - it will return an array of results or null
-
-reg = /paras/g
-// let result3 = reg.match() //ALERT this is wrong
-let result3 = str.match(reg) //this is right
-// console.log(result3);
-
-// 4. search() - returns index of first match or -1
-
-reg = /done/
-// reg = /paradds/
-// let result4 = reg.search(str) //ALERT this is wrong
-let result4 = str.search(reg) //ALERT this is right
-// console.log(result4);
-
-// 5. replace() - return new replaced string with all the replacements (if no flag is given, first match will be replaced)
-
-reg = /done/g
-let result5 = str.replace(reg, 'CODING')
-console.log(result5)
+ if (reg.test(str)) {
+    console.log(`the string ${str} matches the expression ${reg.source}`);
+ } else {
+    console.log(`the string ${str} does not match the expression ${reg.source}`);
+    
+ }
